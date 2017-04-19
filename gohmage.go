7843d446765ba09e104511e10098cd0c66f438d5
Kapeel Sable <kapeel.sable@gmail.com>
2017-04-18 22:15:25 -0700

    Added the /user_info/read ohmage API

--- gohmage.go
+++ gohmage.go
@@ -3,9 +3,8 @@ package gohmage
 import (
   "net/http"
   "net/url"
-  "fmt"
   "io/ioutil"
-  "errors"
+  "github.com/pkg/errors"
   "github.com/antonholmquist/jason"
 )
 
@@ -14,41 +13,60 @@ type Client struct {
   Ohmage_Client string
   Auth_Token string
   Is_Authenticated bool
+  Username string
 }
 
 func NewClient( ohmage_url, client string ) *Client {
   // todo: check trailing slash remove it if present
-  fmt.Println("")
   return &Client {
     Ohmage_URL: ohmage_url,
     Ohmage_Client: client,
     Auth_Token: "",
     Is_Authenticated: false,
+    Username: "",
   }
 }
 
-func ( client *Client ) UserAuthToken( username string, password string ) bool {
+func ( client *Client ) UserAuthToken( username string, password string ) ( bool, error ) {
   parameters := url.Values{ }
   parameters.Set( "user", username )
   parameters.Set( "password", password )
-  response, err := client.request( "post", "/user/auth_token", parameters )
-  if( err != nil ) {
-    // todo : handle call-specific errors: auth success or failure
-    return false;
+  response, err := client.request( "post", "/user/auth_token", parameters, false )
+  if err != nil {
+    return false, errors.Wrap( err, "Ohmage API authorization call failed" );
   }
   result, err := response.GetString( "result" )
   token, err := response.GetString( "token" )
   if result == "success" && token != "" {
     client.Auth_Token = token
     client.Is_Authenticated = true
-    return true;
+    client.Username = username
+    return true, nil;
   }
-  return false;
+  return false, errors.New( "User authentication failed" )
 }
 
-func ( client *Client ) request( method string, endpoint string, parameters url.Values ) (*jason.Object, error) {
+func ( client *Client ) UserInfoRead( ) ( *jason.Object, error ) {
+  response, err := client.request( "post", "/user_info/read", url.Values{ }, true )
+  if err != nil {
+    return nil, errors.Wrap( err, "Ohmage API error occurred" )
+  }
+  return response, nil
+}
+
+func ( client *Client ) request(  method string,
+                                  endpoint string,
+                                  parameters url.Values,
+                                  is_protected_endpoint bool ) ( *jason.Object, error ) {
   var resp *http.Response
   var err error
+  if is_protected_endpoint {
+    if client.Is_Authenticated {
+      parameters.Set( "auth_token", client.Auth_Token )
+    } else {
+      return nil, errors.New( "Authorization token not found" )
+    }
+  }
   parameters.Set( "client", client.Ohmage_Client )
   if method == "post" {
     resp, err = http.PostForm( client.Ohmage_URL + endpoint, parameters )
@@ -57,7 +75,7 @@ func ( client *Client ) request( method string, endpoint string, parameters url.
   }
   defer resp.Body.Close( )
   if( err != nil ) {
-    return nil, errors.New( "An HTTP protocol error occurred." )
+    return nil, errors.New( "An HTTP protocol error occurred" )
   }
   body, err := ioutil.ReadAll( resp.Body )
   if( err != nil ) {
--- gohmage_test.go
+++ gohmage_test.go
@@ -3,16 +3,23 @@ package gohmage
 import (
   "os"
   "testing"
+  "fmt"
   "github.com/stretchr/testify/assert"
 )
 
 var ohmage_url, ohmage_username, ohmage_password string
 const ohmage_client = "gohmage-test"
 
-func TestMain(m *testing.M) {
+func TestMain( m *testing.M ) {
   ohmage_url = os.Getenv( "OHMAGE_URL" )
   ohmage_username = os.Getenv( "OHMAGE_USERNAME" )
   ohmage_password = os.Getenv( "OHMAGE_PASSWORD" )
+  if ohmage_url == "" || ohmage_username == "" || ohmage_password == "" {
+    fmt.Println( "Ohmage server environment variables not set. Cannot proceed with tests." )
+    os.Exit( 1 )
+  } else {
+    os.Exit( m.Run( ) )
+  }
 }
 
 func TestClient( t *testing.T ) {
@@ -24,11 +31,27 @@ func TestClient( t *testing.T ) {
 func TestApi( t *testing.T ) {
   client := NewClient( ohmage_url, ohmage_client )
   t.Run( "UserAuthToken", client.testUserAuthToken )
+  t.Run( "UserInfoRead", client.testUserInfoRead )
 }
 
-func (client *Client) testUserAuthToken( t *testing.T ) {
+func ( client *Client ) testUserAuthToken( t *testing.T ) {
   assert.Equal( t, client.UserAuthToken( ohmage_username, ohmage_password ), true, "Auth should succeed" )
   assert.Equal( t, client.Is_Authenticated, true, "Is_Authenticated should be true" )
   assert.NotEqual( t, client.Auth_Token, "", "Auth_Token should not be empty" )
 }
 
+func ( client *Client ) testUserInfoRead( t *testing.T ) {
+  response, err := client.UserInfoRead( )
+  if err != nil {
+    t.Error( "Failed to read JSON response" )
+  }
+  classes, err := response.GetObject( "data", ohmage_username, "classes" )
+  if err != nil {
+    t.Error( "Failed to find the classes attribute in JSON response" )
+  }
+  public_class, err := classes.GetString( "urn:class:public" )
+  if err != nil {
+    t.Error( "Failed to find the public class attribute in the JSON response" )
+  }
+  assert.Equal( t, public_class, "Public Class", "Public Class URN should match" )
+}
